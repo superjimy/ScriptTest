57c0c985447fb5cf5904dd3222c25c6f438d6147
superjimy <tadashi.azegami@gmail.com>
2017-12-10 07:22:35 +0900

    初めてのScriptTest
    Scriptの使い方の最初のファイルです

---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour {
+
+	//Use this for initialization
+	void Start () {
+
+		//要素数5の配列を初期化する
+	int[] array = new int[5];
+
+	//配列の各要素に値を代入する
+	array [0] = 30;
+	array [1] = 10;
+	array [2] = 25;
+	array [3] = 40;
+	array [4] = 55;
+
+	//配列の要素を全て表示する
+	for (int i = 4; i >= 0 ; i--) {
+		Debug.Log (array [i]);
+	}
+}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
