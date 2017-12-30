5a76a1c110d0fbfd3166b74b4f33ea6905074bf2
superjimy <tadashi.azegami@gmail.com>
2017-12-30 16:12:31 +0900

    提出用

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,26 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss {
+	private int hp = 100; //体力
+	private int power = 25; //攻撃力
+	private int mp = 53; //魔法力
+	private int mpower = 5; //魔法攻撃力
+	//攻撃の関数
+	public void Attack(){
+		Debug.Log ("拳で" + this.power + "のダメージを与えた");
+		}
+	//魔法の関数
+	public void Magic(){
+		Debug.Log ("魔法で" + this.mpower + "のダメージを与えた");
+		}
+
+	//防御用の関数
+	public void Defence(int damage){
+		Debug.Log (damage + "のダメージを受けた");
+		//残りのhpを減らす
+		this.hp -= damage;
+	}
+}
 
 public class Test : MonoBehaviour {
 
 	//Use this for initialization
 	void Start () {
+		//Bossクラスの変数を宣言してインスタンスを代入
+		Boss Lastboss = new Boss ();
 
-		//要素数5の配列を初期化する
-	int[] array = new int[5];
-
-	//配列の各要素に値を代入する
-	array [0] = 30;
-	array [1] = 10;
-	array [2] = 25;
-	array [3] = 40;
-	array [4] = 55;
-
-	//配列の要素を全て表示する
-	for (int i = 4; i >= 0 ; i--) {
-		Debug.Log (array [i]);
-	}
+		//攻撃用の関数を呼び出す
+		Lastboss.Attack();
+		//防御用の関数を呼び出す
+		Lastboss.Defence(3);
+		//魔法用の関数を呼び出す
+		Lastboss.Magic();
 }
 		
 	// Update is called once per frame
